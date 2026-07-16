9279ce6e7546918d3e606cc4965a3394bc85742a
center783-tech <center783@gmail.com>
2026-07-16 22:15:32 +0800

    Merge commit 'e2aa590176418008e31f253fa6caeb627d1cf062' into admin
    (cherry picked from commit ba4c9f295d2700f04cf8f9cf436351ecb4e4a802)

--- src/App.java
+++ src/App.java
@@ -1,7 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+<<<<<<< HEAD
         System.out.println("main-update-#1-#2");
         System.out.println("dev-update-#1");
+=======
+<<<<<<< HEAD
+        System.out.println("main-update-#1");
+        System.out.println("admin-update-#1");
+=======
+        System.out.println("main-update-#1-#2");
+        System.out.println("dev-update-#1");
+>>>>>>> e2aa590176418008e31f253fa6caeb627d1cf062
+>>>>>>> ba4c9f2 (Merge commit 'e2aa590176418008e31f253fa6caeb627d1cf062' into admin)
     }
 }
